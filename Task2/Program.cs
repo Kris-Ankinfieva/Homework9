05f27eada0f1c772380d1eec27adb3db6183da6c
Kris Ankinfieva <kk397758@yandex.ru>
2022-11-28 14:04:29 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Программа, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Clear();
+int numM = Prompt("Введите число M");
+int numN = Prompt("Введите число N");
+
+int Prompt(string msg)
+{
+    Console.Write(msg + " > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int SumNumbers(int numberM, int numberN)
+{
+    int sum = numberM;
+    if (numberN == numberM)
+    {
+        return 0;
+    }
+    else
+    {
+        numberM++;
+        sum = numberM + SumNumbers(numberM, numberN);
+        return sum;
+    }
+}
+void PrintSumNumber(int valueM, int valueN)
+{
+    Console.Write(SumNumbers(valueM - 1, valueN));
+}
+PrintSumNumber(numM, numN);
